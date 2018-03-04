7aeb9fb91219881563f2a681bebce7824512ea09
Opeyemi Olorunleke <edgedeveloper007@gmail.com>
2018-03-04 04:34:45 +0100

    Update! This is a very simple yet sophisticate way to implement multiselection on a recyclerView without losing the cool animation effect (of a recyclerView!)

--- app/src/main/java/com/edgedevstudio/example/recyclerviewmultiselect/MainActivity.kt
+++ app/src/main/java/com/edgedevstudio/example/recyclerviewmultiselect/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.edgedevstudio.example.recyclerviewmultiselect
 
-import android.content.Context
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.view.*
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
-import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity(), MainInterface {
@@ -106,83 +105,7 @@ class MainActivity : AppCompatActivity(), MainInterface {
 
     fun getRandomID() = UUID.randomUUID().toString()
 
-    class MyAdapter(val context: Context, val mainInterface: MainInterface) : RecyclerView.Adapter<MyViewHolder>(), RecyclerViewClick {
-        override fun onLongTap(index: Int) {
-            if (!isMultiSelectOn) {
-                isMultiSelectOn = true
-            }
-            addIDIntoSelectedIds(index)
-        }
 
-        override fun onTap(index: Int) {
-            if (isMultiSelectOn) {
-                addIDIntoSelectedIds(index)
-            } else {
-                Toast.makeText(context, "Clicked Item @ Position ${index + 1}", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        fun addIDIntoSelectedIds(index: Int) {
-            val id = modelList[index].id
-            if (selectedIds.contains(id))
-                selectedIds.remove(id)
-            else
-                selectedIds.add(id)
-
-            notifyItemChanged(index)
-            if (selectedIds.size < 1) isMultiSelectOn = false
-            mainInterface.mainInterface(selectedIds.size)
-        }
-
-        var modelList: MutableList<MyModel> = ArrayList<MyModel>()
-        val selectedIds: MutableList<String> = ArrayList<String>()
-
-        override fun getItemCount() = modelList.size
-
-        override fun onBindViewHolder(holder: MyViewHolder?, index: Int) {
-            holder?.textView?.setText(modelList[index].title)
-
-            val id = modelList[index].id
-
-            if (selectedIds.contains(id)) {
-                //if item is selected then,set foreground color of FrameLayout.
-                holder?.frameLayout?.foreground = ColorDrawable(ContextCompat.getColor(context, R.color.colorControlActivated))
-            } else {
-                //else remove selected item color.
-                holder?.frameLayout?.foreground = ColorDrawable(ContextCompat.getColor(context, android.R.color.transparent))
-            }
-        }
-
-        fun deleteSelectedIds() {
-            if (selectedIds.size < 1) return
-            val selectedIdIteration: MutableListIterator<String> = selectedIds.listIterator();
-
-            while (selectedIdIteration.hasNext()) {
-                val selectedItemID = selectedIdIteration.next()
-                Log.d(TAG, "The ID is $selectedItemID")
-                var indexOfModelList = 0
-                val modelListIteration: MutableListIterator<MyModel> = modelList.listIterator();
-                while (modelListIteration.hasNext()) {
-                    val model = modelListIteration.next()
-                    if (selectedItemID.equals(model.id)) {
-                        modelListIteration.remove()
-                        selectedIdIteration.remove()
-                        notifyItemRemoved(indexOfModelList)
-                    }
-                    indexOfModelList++
-                }
-
-                isMultiSelectOn = false
-            }
-        }
-
-
-        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyViewHolder {
-            val inflater = LayoutInflater.from(parent?.context)
-            val itemView = inflater.inflate(R.layout.view_holder_layout, parent, false)
-            return MyViewHolder(itemView, this)
-        }
-    }
 
     class MyViewHolder(itemView: View, val r_tap: RecyclerViewClick) : RecyclerView.ViewHolder(itemView),
             View.OnLongClickListener, View.OnClickListener {
---
+++ app/src/main/java/com/edgedevstudio/example/recyclerviewmultiselect/MyAdapter.kt
@@ -0,0 +1,92 @@
+package com.edgedevstudio.example.recyclerviewmultiselect
+
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import java.util.*
+
+/**
+ * Created by OPEYEMI OLORUNLEKE on 3/4/2018.
+ */
+class MyAdapter(val context: Context, val mainInterface: MainInterface) : RecyclerView.Adapter<MainActivity.MyViewHolder>(), RecyclerViewClick {
+    override fun onLongTap(index: Int) {
+        if (!MainActivity.isMultiSelectOn) {
+            MainActivity.isMultiSelectOn = true
+        }
+        addIDIntoSelectedIds(index)
+    }
+
+    override fun onTap(index: Int) {
+        if (MainActivity.isMultiSelectOn) {
+            addIDIntoSelectedIds(index)
+        } else {
+            Toast.makeText(context, "Clicked Item @ Position ${index + 1}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun addIDIntoSelectedIds(index: Int) {
+        val id = modelList[index].id
+        if (selectedIds.contains(id))
+            selectedIds.remove(id)
+        else
+            selectedIds.add(id)
+
+        notifyItemChanged(index)
+        if (selectedIds.size < 1) MainActivity.isMultiSelectOn = false
+        mainInterface.mainInterface(selectedIds.size)
+    }
+
+    var modelList: MutableList<MyModel> = ArrayList<MyModel>()
+    val selectedIds: MutableList<String> = ArrayList<String>()
+
+    override fun getItemCount() = modelList.size
+
+    override fun onBindViewHolder(holder: MainActivity.MyViewHolder?, index: Int) {
+        holder?.textView?.setText(modelList[index].title)
+
+        val id = modelList[index].id
+
+        if (selectedIds.contains(id)) {
+            //if item is selected then,set foreground color of FrameLayout.
+            holder?.frameLayout?.foreground = ColorDrawable(ContextCompat.getColor(context, R.color.colorControlActivated))
+        } else {
+            //else remove selected item color.
+            holder?.frameLayout?.foreground = ColorDrawable(ContextCompat.getColor(context, android.R.color.transparent))
+        }
+    }
+
+    fun deleteSelectedIds() {
+        if (selectedIds.size < 1) return
+        val selectedIdIteration: MutableListIterator<String> = selectedIds.listIterator();
+
+        while (selectedIdIteration.hasNext()) {
+            val selectedItemID = selectedIdIteration.next()
+            Log.d(MainActivity.TAG, "The ID is $selectedItemID")
+            var indexOfModelList = 0
+            val modelListIteration: MutableListIterator<MyModel> = modelList.listIterator();
+            while (modelListIteration.hasNext()) {
+                val model = modelListIteration.next()
+                if (selectedItemID.equals(model.id)) {
+                    modelListIteration.remove()
+                    selectedIdIteration.remove()
+                    notifyItemRemoved(indexOfModelList)
+                }
+                indexOfModelList++
+            }
+
+            MainActivity.isMultiSelectOn = false
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MainActivity.MyViewHolder {
+        val inflater = LayoutInflater.from(parent?.context)
+        val itemView = inflater.inflate(R.layout.view_holder_layout, parent, false)
+        return MainActivity.MyViewHolder(itemView, this)
+    }
+}
